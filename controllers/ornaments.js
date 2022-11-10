ac79a89dae2f9befad48d03c80aba72eeb3d21a2
saida-reddy <108296645+saida-reddy@users.noreply.github.com>
2022-11-09 19:49:45 -0600

    updated

--- controllers/ornaments.js
+++ controllers/ornaments.js
@@ -19,8 +19,24 @@ exports.ornaments_detail = function(req, res) {
 }; 
  
 // Handle Costume create on POST. 
-exports.ornaments_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Ornaments create POST'); 
+exports.ornaments_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Ornaments(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+     //{"OrnamentType":"Gold", "OrnamentCost":500, "OrnamentStrength":"High"} 
+    document.OrnamentType = req.body.OrnamentType; 
+    document.OrnamentCost = req.body.OrnamentCost; 
+    document.OrnamentStrength = req.body.OrnamentStrength; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume delete form on DELETE. 
